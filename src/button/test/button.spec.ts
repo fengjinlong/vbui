b0e7f4c4e8103514d4d51b919b0c99a853d49ecf
fjl <18618166564@163.com>
2022-10-11 22:06:15 +0800

    nb

--- src/button/test/button.spec.ts
+++ src/button/test/button.spec.ts
@@ -44,7 +44,7 @@ describe("color", () => {
       wrapper
         .classes()
         .map((v) => v.replace("\n", ""))
-        .includes("bg-red-5009")
+        .includes("bg-red-500")
     ).toBe(true);
   });
 });
